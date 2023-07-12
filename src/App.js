8c8cb1edfefafe703b2f2e88ef0999464f82d979
Carlos Gutierrez <cmgutierrez34@misena.edu.co>
2023-07-12 18:58:03 -0500

    Deploy on ghpages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={ <Home/> } />
         <Route path="/results/:search" element={<Results/>} />
---
+++ src/utils/config.js
@@ -0,0 +1,5 @@
+const config = {
+  currentPage: 1,
+  itemsPerPage: 5,
+};
+export default config;
